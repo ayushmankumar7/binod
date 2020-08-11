7a301bcfa5a57509d5836a20643c3908084fd7c9
Eshan Chatterjee <eshan2001221@gmail.com>
2020-08-11 11:01:46 +0530

    added search support and infinite binod feature

--- binod/tharu.py
+++ binod/tharu.py
@@ -1,4 +1,34 @@
+import os
+
 def hello():
     return "binod"
 
 
+def InfyHello():
+    bin = True
+    while bin:
+        print(hello())
+
+
+def isBinod(filename):
+    with open(filename, "r") as f:
+        fileContent = f.read()
+    if "binod" in fileContent.lower():   #binod is very tricky to find, Detect all BiNoDs
+        return True
+    return False
+
+
+
+def SearchBinod():
+    #listing the contents of this dir
+    DirContents = os.listdir()
+
+    #for each .txt file detect
+    for item in DirContents:
+        if item.endswith('.txt'):
+            print(f"Detecting Binod!")
+            flag = isBinod(item)
+            if (flag):
+                print(f"Binod Found in {item}")
+            else:
+                print(f"Binod Not Found")
--- test.py
+++ test.py
@@ -1,4 +1,18 @@
-from binod.tharu import hello 
+from binod.tharu import InfyHello 
+from binod.tharu import hello
+from binod.tharu import isBinod
+from binod.tharu import SearchBinod
+
+
 
 #binod
-print(hello())
+print("Press 1 to Print")
+print("Press 2 to Print Infinitely")
+print("Press 4 to Search a dir For Hidden Binod files")
+choice = input()
+if(int(choice) == 1):
+    print(hello())
+elif(int(choice) == 2):
+    InfyHello()
+elif (int(choice) == 4):
+    SearchBinod()
